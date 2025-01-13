8e4c2b19de7a75017e8062d9253fc7df42b6e9bf
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-01-13 09:17:26 +0530

    added minute changes

--- Client-side/static-js/auth.js
+++ Client-side/static-js/auth.js
@@ -1,31 +1,47 @@
-let signupBtn = document.getElementById('signup-btn');
-        let loginBtn = document.getElementById('login-btn');
-        let formHeading = document.getElementById('form-heading');
-        let suggestion = document.getElementById('suggestion');
-        let loginLink = document.getElementById('login-link');
-        let forgotPassword = document.getElementById('forgot-password');
+let signupBtn = document.getElementById("signup-btn");
+let loginBtn = document.getElementById("login-btn");
+let formHeading = document.getElementById("form-heading");
+let suggestion = document.getElementById("suggestion");
+let loginLink = document.getElementById("login-link");
+let forgotPassword = document.getElementById("forgot-password");
 
-        function showSignupForm() {
-            signupBtn.classList.add('active');
-            loginBtn.classList.remove('active');
-            document.getElementById('email-group').style.display = 'block';
-            document.getElementById('submit-btn').innerHTML = 'Signup';
-            suggestion.innerHTML = 'Already have an account? <a href="#" class="login-link" id="login-link">Login</a>';
-            formHeading.innerHTML = 'Signup Form';
-            document.getElementById('login-link').addEventListener('click', showLoginForm);
-            forgotPassword.style.display = 'none';
-        }
 
-        function showLoginForm() {
-            loginBtn.classList.add('active');
-            signupBtn.classList.remove('active');
-            document.getElementById('email-group').style.display = 'none';
-            document.getElementById('submit-btn').innerHTML = 'Login';
-            suggestion.innerHTML = 'Don’t have an account? <a href="#" class="signup-link" id="signup-link">Signup</a>';
-            formHeading.innerHTML = 'Login Form';
-            document.getElementById('signup-link').addEventListener('click', showSignupForm);
-            forgotPassword.style.display = 'block';
-        }
-        signupBtn.addEventListener('click', showSignupForm);
-        loginBtn.addEventListener('click', showLoginForm);
-        loginLink.addEventListener('click', showLoginForm);
+function showSignupForm() {
+  signupBtn.classList.add("active");
+  loginBtn.classList.remove("active");
+  document.getElementById("email-group").style.display = "block";
+  document.getElementById("submit-btn").innerHTML = "Signup";
+  suggestion.innerHTML =
+    'Already have an account? <a href="#" class="login-link" id="login-link">Login</a>';
+  formHeading.innerHTML = "Signup Form";
+  document
+    .getElementById("login-link")
+    .addEventListener("click", showLoginForm);
+  forgotPassword.style.display = "none";
+}
+
+function showLoginForm() {
+  loginBtn.classList.add("active");
+  signupBtn.classList.remove("active");
+  document.getElementById("email-group").style.display = "none";
+  document.getElementById("submit-btn").innerHTML = "Login";
+  suggestion.innerHTML =
+    'Don’t have an account? <a href="#" class="signup-link" id="signup-link">Signup</a>';
+  formHeading.innerHTML = "Login Form";
+  document
+    .getElementById("signup-link")
+    .addEventListener("click", showSignupForm);
+  forgotPassword.style.display = "block";
+}
+signupBtn.addEventListener("click", showSignupForm);
+loginBtn.addEventListener("click", showLoginForm);
+loginLink.addEventListener("click", showLoginForm);
+
+
+forgotPassword.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.location.href = "resetpassword.html";
+  localStorage.setItem("email", document.getElementById("email").value);
+  localStorage.setItem('username', document.getElementById("username").value);
+
+});
